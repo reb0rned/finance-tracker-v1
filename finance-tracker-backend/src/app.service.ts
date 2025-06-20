68903f42d3772030e83bc2c47829fae2ca04ce71
reb0rned <affmarket77@gmail.com>
2025-06-20 16:03:53 +0300

    entities added + typeorm initialized

--- finance-tracker-backend/src/app.service.ts
+++ finance-tracker-backend/src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUser(): string {
+    return 'Here is your User!';
+  }
 }
--- finance-tracker-backend/src/user/entities/user.entity.ts
+++ finance-tracker-backend/src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Category } from "src/category/entities/category.entity";
+import { Transaction } from "src/transaction/entities/transaction.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @OneToMany(() => Category, (category) => category.user, {
+    onDelete: 'CASCADE' // если удаляем юзера то удаляеться и категория
+  })
+  categories: Category[]
+
+  @OneToMany(() => Transaction, (transaction) => transaction.user, {
+    onDelete: 'CASCADE' // если удаляем юзера то удаляеться и категория
+  })
+  transactions: Transaction[]
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+}
